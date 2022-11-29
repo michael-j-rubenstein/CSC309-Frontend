10263f2aa69807873cdd9081e2dc488aa6f79860
Michael J R <michael.rubenstein@mail.utoronto.ca>
2022-11-29 14:09:19 -0500

    updated seo

--- src/components/Layout/Nav.js
+++ src/components/Layout/Nav.js
@@ -3,14 +3,14 @@ import { Link, Outlet } from "react-router-dom";
 const Nav = () => {
   return (
     <>
-      <div>
+      <nav>
         <Link to="/">Home</Link>
         <Link to="/me/">Me</Link>
         <Link to="/subscription/">Subscriptions</Link>
         <Link to="/signup/">Signup</Link>
         <Link to="/login/">Signin</Link>
         <Link to="/admin/">Admin</Link>
-      </div>
+      </nav>
 
       <Outlet />
     </>
