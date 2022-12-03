f2a9d363d8c05f551739df9f99108e1e19ae56f1
Michael J R <michael.rubenstein@mail.utoronto.ca>
2022-12-03 13:08:57 -0500

    added studio item component

---
+++ src/components/Studio/StudioItem.js
@@ -0,0 +1,35 @@
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Button from "../UI/Button";
+
+import styles from "./StudioItem.module.css";
+
+const StudioItem = (props) => {
+  const { name, address, distance, directions } = props.data;
+
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles["studio-wrapper"]}>
+      <h2 className={styles["studio-name"]}>{name}</h2>
+      <p className={styles["studio-address"]}>{address}</p>
+      <div className={styles["studio-other"]}>
+        <p className={styles["studio-distance"]}>{distance} kilometers away</p>
+        <div className={styles["studio-btn-wrapper"]}>
+          <a href={directions} className={styles["btn-link"]} target="_blank">
+            <Button className={styles["studio-btn"]} btnColor="plain">
+              Directions
+            </Button>
+          </a>
+          <Link>
+            <Button className={styles["studio-btn"]} btnColor="plain">
+              View
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StudioItem;
--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -6,6 +6,7 @@ import Title from "../../components/Text/Title";
 import Card from "../../components/Layout/Card";
 
 import styles from "./HomePage.module.css";
+import StudioItem from "../../components/Studio/StudioItem";
 
 const HomePage = () => {
   const latInputRef = useRef();
@@ -15,13 +16,33 @@ const HomePage = () => {
   const locationSubmitHandler = (event) => {
     event.preventDefault();
     console.log(latInputRef.current.value, longInputRef.current.value);
+    setStudios({
+      "GoodLife Fitness Toronto Richmond and John": {
+        id: 1,
+        name: "GoodLife Fitness Toronto Richmond and John",
+        address: "267 Richmond St W, Toronto, ON",
+        distance: 12,
+        directions:
+          "https://www.google.com/maps/dir/633+Bay+St.,+Toronto,+ON+M5G+2G4,+Canada/GoodLife+Fitness+Toronto+Richmond+and+John,+267+Richmond+St+W,+Toronto,+ON+M5V+3M6/@43.648754,-79.4094495,15z/data=!4m14!4m13!1m5!1m1!1s0x882b34ca53f5a36b:0x9c6da32c964a3a2!2m2!1d-79.3832236!2d43.6567671!1m5!1m1!1s0x882b34da9f48e43b:0x17f1fdd1a8210e42!2m2!1d-79.39194!2d43.648754!3e3",
+      },
+      "second studio": {
+        id: 1,
+        name: "9Round Fitness",
+        address: "777 Bay St. M219, Toronto, ON",
+        distance: 2,
+        directions:
+          "https://www.google.com/maps/dir/633+Bay+St.,+Toronto,+ON+M5G+2G4,+Canada/9Round+Fitness,+777+Bay+St.+M219,+Toronto,+ON+M7A+2J3/@43.6587967,-79.3853891,17z/data=!3m2!4b1!5s0x882b34cb7491effd:0xadf4bc377f6e1d05!4m14!4m13!1m5!1m1!1s0x882b34ca53f5a36b:0x9c6da32c964a3a2!2m2!1d-79.3832236!2d43.6567671!1m5!1m1!1s0x882b357a658f9ed5:0x3a21ed7558a2816!2m2!1d-79.3841909!2d43.6606226!3e3",
+      },
+    });
   };
 
-  const studio_list = studios.map((studio) => {
-    console.log(studio);
+  var studio_list = Object.keys(studios);
+  studio_list = studio_list.map((studio_name) => {
+    const data = studios[`${studio_name}`];
+    return <StudioItem id={data.id} data={{ ...data }}></StudioItem>;
   });
 
-  console.log(studio_list);
+  // console.log(studio_list);
 
   return (
     <>
