ffd1cd3199890398a8eedde4f8bcef6fc1d16a17
Michael J R <michael.rubenstein@mail.utoronto.ca>
2022-12-03 23:17:44 -0500

    done with home

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -21,7 +21,7 @@ const HomePage = () => {
 
     var bearer =
       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcwMTI3OTgxLCJpYXQiOjE2NzAxMjQzODEsImp0aSI6IjAzNGJmODc3NzdmYzRiODQ4NDExMTNhMzc3ZWI1ZDU0IiwidXNlcl9pZCI6Nn0.uyjPV3bJ3g5jB2LY7Ele6qw0e3-fkI5ZcuGNabsjTnQ";
-    console.log(bearer);
+    console.log(latInputRef.current.value);
 
     const config = {
       headers: {
@@ -29,12 +29,15 @@ const HomePage = () => {
       },
     };
 
+    const user_latitude = latInputRef.current.value * 1;
+    const user_longitude = longInputRef.current.value * 1;
+
     axios
       .post(
         `${process.env.REACT_APP_BACKEND_URL}studios/`,
         {
-          latitude: 1,
-          longitude: 2,
+          latitude: user_latitude,
+          longitude: user_longitude,
           mode: "no-cors",
         },
         config
@@ -50,24 +53,6 @@ const HomePage = () => {
     // });
 
     setSearched(true);
-    setStudios({
-      "GoodLife Fitness Toronto Richmond and John": {
-        id: 1,
-        name: "GoodLife Fitness Toronto Richmond and John",
-        address: "267 Richmond St W, Toronto, ON",
-        distance: 12,
-        directions:
-          "https://www.google.com/maps/dir/633+Bay+St.,+Toronto,+ON+M5G+2G4,+Canada/GoodLife+Fitness+Toronto+Richmond+and+John,+267+Richmond+St+W,+Toronto,+ON+M5V+3M6/@43.648754,-79.4094495,15z/data=!4m14!4m13!1m5!1m1!1s0x882b34ca53f5a36b:0x9c6da32c964a3a2!2m2!1d-79.3832236!2d43.6567671!1m5!1m1!1s0x882b34da9f48e43b:0x17f1fdd1a8210e42!2m2!1d-79.39194!2d43.648754!3e3",
-      },
-      "second studio": {
-        id: 2,
-        name: "9Round Fitness",
-        address: "777 Bay St. M219, Toronto, ON",
-        distance: 2,
-        directions:
-          "https://www.google.com/maps/dir/633+Bay+St.,+Toronto,+ON+M5G+2G4,+Canada/9Round+Fitness,+777+Bay+St.+M219,+Toronto,+ON+M7A+2J3/@43.6587967,-79.3853891,17z/data=!3m2!4b1!5s0x882b34cb7491effd:0xadf4bc377f6e1d05!4m14!4m13!1m5!1m1!1s0x882b34ca53f5a36b:0x9c6da32c964a3a2!2m2!1d-79.3832236!2d43.6567671!1m5!1m1!1s0x882b357a658f9ed5:0x3a21ed7558a2816!2m2!1d-79.3841909!2d43.6606226!3e3",
-      },
-    });
   };
 
   var studio_list = Object.keys(studios);
