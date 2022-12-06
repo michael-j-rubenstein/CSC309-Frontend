6334d68eb6403e854c57de16705613f4c7ebaadd
ayanaarahman <ayanaa.rahman@mail.utoronto.ca>
2022-12-06 10:45:15 -0500

    updating

--- src/pages/Login/LoginPage.js
+++ src/pages/Login/LoginPage.js
@@ -1,25 +1,47 @@
 import { Link } from "react-router-dom";
 import styles from "./LoginPage.module.css";
+import axios from "axios";
+import React, { useState } from 'react';
 
 const LoginPage = () => {
-  const dosomething = (event) => {
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const LoginPage = (event) => {
     event.preventDefault();
-    console.log("hello");
+    axios
+      .post(
+        `${process.env.REACT_APP_BACKEND_URL}accounts/login/`,
+        {
+          username: username,
+          password: password,
+          mode: "no-cors",
+        },
+        {}
+      )
+      .then((res) => {
+        console.log(res.data);
+        let token = res.data.access;
+        localStorage.setItem("SavedToken", 'Bearer ' + token);
+        axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+      });
+
   };
   return (
-    <form className={styles.form} onSubmit={dosomething}>
+    <form className={styles.form} onSubmit={LoginPage}>
       <div className={styles["form-content"]}>
         <h1 className={styles["form-title"]}>Please sign in</h1>
         <input
-          type="email"
-          placeholder="Email address"
+          type="username"
+          placeholder="Username"
           autoFocus
           className={styles["form-input"]}
+          value={username} onChange={(e) => setUserName(e.target.value)}
         ></input>
         <input
           type="password"
           placeholder="Password"
           className={styles["form-input"]}
+          value={password} onChange={(e) => setPassword(e.target.value)}
         ></input>
         <Link to="/signup/" className={styles["form-redirect"]}>
           Don't have an account?
