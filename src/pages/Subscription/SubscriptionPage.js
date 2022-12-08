30bfcce68be4a9678a8b4525722c84114efab2f2
ayanaarahman <ayanaa.rahman@mail.utoronto.ca>
2022-12-08 14:58:03 -0500

    adding

--- src/pages/Subscription/SubscriptionPage.js
+++ src/pages/Subscription/SubscriptionPage.js
@@ -1,4 +1,39 @@
 const SubscriptionPage = () => {
+  const [userInfo, setUserInfo] = useState({});
+
+  var bearer = localStorage.getItem("SavedToken");
+
+  useEffect(() => {
+    const config = {
+      headers: {
+        Authorization: `${bearer}`,
+      },
+    };
+
+    if (bearer) {
+      // get all subscription data
+
+      axios
+        .get(`${process.env.REACT_APP_BACKEND_URL}subscriptions`, config)
+        .then((res) => {
+          console.log(res.data);
+          const data = res.data;
+          setUserInfo({
+            fname: data.first_name ? data.first_name : "",
+            lname: data.last_name ? data.last_name : "",
+            email: data.email ? data.email : "",
+            phone: data.phone_number ? data.phone_number * 1 : "",
+            avatar: data.avatar ? data.avatar : "",
+          });
+        });
+    }
+  }, [bearer]);
+
+  const [form, setForm] = useState("profile");
+
+  const selectHandler = (event) => {
+    setForm(event.target.value);
+  };
   return <h2>This is SubscriptionPage</h2>;
 };
 
